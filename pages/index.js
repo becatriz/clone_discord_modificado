507d2795d4a11ef21e55f64fd349e54e13b0150d
Rebeca <rebeca.cruz@before.com.br>
2022-01-25 22:54:33 -0400

    fix: retira estilo global do index 🔧

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,34 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { Loading } from "../components/Loading";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import appConfig from "../config.json";
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css'; 
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const route = useRouter()
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 2500);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!name) return toast.error('Preencha seu nome de usuário');
+
+    route.push(`chat/${name}`)
+  }
+
   return (
-    <>
-      <GlobalStyle />
+    <> 
       <Box
         styleSheet={{
           display: "flex",
@@ -50,136 +42,157 @@ export default function Home() {
           backgroundBlendMode: "multiply",
         }}
       >
-        
-        <Box
-          styleSheet={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            flexDirection: {
-              xs: "column",
-              sm: "row",
-            },
-            width: "100%",
-            maxWidth: "400px",
-            borderRadius: "5px",
-            padding: "32px",
-            margin: "16px",
-            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
-            backgroundColor: appConfig.theme.colors.neutrals[700],
-          }}
-        >
-          
-          {/* Formulário */}
+        <div className={styles["form-div"]}>
+        <ToastContainer autoClose={3000}/>
           <Box
-            as="form"
             styleSheet={{
               display: "flex",
-              flexDirection: "column",
               alignItems: "center",
-              justifyContent: "center",
-              width: { xs: "100%", sm: "100%" },
-              textAlign: "left",
-              marginBottom: "32px",
+              justifyContent: "space-between",
+              flexDirection: {
+                xs: "column",
+                sm: "row",
+              },
+              width: "100%",
+              maxWidth: "400px",
+              borderRadius: "5px",
+              padding: "32px",
+              margin: "16px",
+              boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+              backgroundColor: appConfig.theme.colors.neutrals[700],
             }}
           >
-             <Image
-              styleSheet={{
-                width: "60px",
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
-              src={`https://github.com/${"becatriz"}.png`}
-            />
-            <Text
-              variant="body3"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[300],
-                marginBottom: '8px',
-              }}
-            >
-              Rebeca Lopes convidou você para se juntar
-            </Text>
-            <Text
-              variant="body1"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals["000"],
-              }}
-            >
-              {appConfig.name}
-            </Text>
-
+            {/* Formulário */}
             <Box
+              as="form"
+              onSubmit={(e) => handleSubmit(e)}
               styleSheet={{
-                marginBottom: "22px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: { xs: "100%", sm: "100%" },
+                textAlign: "left",
+                marginBottom: "32px",
               }}
             >
-              <Text
-                styleSheet={{
-                  fontSize: "12px",
-                  color: appConfig.theme.colors.neutrals[300],
-                }}
-              >
-                <span
+              {loading ? (
+                <div
                   style={{
-                    display: "inline-block",
-                    color: "green",
-                    fontSize: "20px",
+                    marginBottom: "10px",
                   }}
                 >
-                  &#8226;
-                </span>{" "}
-                150 online
-              </Text>
-
+                  <Loading />
+                </div>
+              ) : (
+                <Box
+                height="150px"
+                width="150px"
+                 >
+                  <Image
+                    styleSheet={{
+                      width: "60px",
+                      borderRadius: "50%",
+                      marginBottom: "16px",
+                    }}
+                    src={`https://github.com/${name}.png`}
+                    
+                  />
+                </Box>
+              )}
               <Text
+                variant="body3"
                 styleSheet={{
-                  marginLeft: "20px",
-                  fontSize: "12px",
                   color: appConfig.theme.colors.neutrals[300],
+                  marginBottom: "8px",
                 }}
               >
-                <span
-                  style={{
-                    color: "#FFFF",
-                    fontSize: "20px",
+                Rebeca Lopes convidou você para se juntar
+              </Text>
+              <Text
+                variant="body1"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals["000"],
+                }}
+              >
+                {appConfig.name}
+              </Text>
+
+              <Box
+                styleSheet={{
+                  marginBottom: "22px",
+                }}
+              >
+                <Text
+                  styleSheet={{
+                    fontSize: "12px",
+                    color: appConfig.theme.colors.neutrals[300],
                   }}
                 >
-                  &#8226;
-                </span>{" "}
-                1.257 membros
-              </Text>
-            </Box>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      color: "green",
+                      fontSize: "20px",
+                    }}
+                  >
+                    &#8226;
+                  </span>{" "}
+                  150 online
+                </Text>
 
-            <TextField
-              label="NOME DE USUÁRIO"
-              fullWidth
-              size="xs"
-              placeholder="Como o pessoal vai te chamar?"
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
-              }}
-              styleSheet={{
-                textAlign: "left",
-              }}
-            />
-            <Button
-              type="submit"
-              label="Continuar"
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
-           
+                <Text
+                  styleSheet={{
+                    marginLeft: "20px",
+                    fontSize: "12px",
+                    color: appConfig.theme.colors.neutrals[300],
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#FFFF",
+                      fontSize: "20px",
+                    }}
+                  >
+                    &#8226;
+                  </span>{" "}
+                  1.257 membros
+                </Text>
+              </Box>
+
+              <TextField
+                label="NOME DE USUÁRIO"
+                fullWidth
+                size="xs"
+                placeholder="Digite seu user do github"
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                  },
+                }}
+                styleSheet={{
+                  textAlign: "left",
+                }}
+                onChange={(e) => {
+                  setName(e.target.value);
+                  setLoading(true);
+                }}
+              />
+              <Button
+                type="submit"
+                label="Continuar"
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+              />
+
               <Text
                 variant="body4"
                 tag="a"
@@ -188,7 +201,7 @@ export default function Home() {
                   color: appConfig.theme.colors.variant.primary[300],
                   width: { xs: "100%", sm: "100%" },
                   textAlign: "left",
-                  marginBottom: '16px',
+                  marginBottom: "16px",
                 }}
               >
                 Já tem uma conta?
@@ -202,18 +215,29 @@ export default function Home() {
                   color: appConfig.theme.colors.neutrals["400"],
                   width: { xs: "100%", sm: "100%" },
                   textAlign: "left",
-                  
                 }}
               >
-                Ao se registar, você concorda com os <span style={{
-                  color: appConfig.theme.colors.variant.primary[300]
-                }}>termos de serviço</span> e a <span style={{
-                  color: appConfig.theme.colors.variant.primary[300]
-                }}>politica de privacidade</span> do Dashboard
+                Ao se registar, você concorda com os{" "}
+                <span
+                  style={{
+                    color: appConfig.theme.colors.variant.primary[300],
+                  }}
+                >
+                  termos de serviço
+                </span>{" "}
+                e a{" "}
+                <span
+                  style={{
+                    color: appConfig.theme.colors.variant.primary[300],
+                  }}
+                >
+                  politica de privacidade
+                </span>{" "}
+                do Dashboard
               </Text>
-           
+            </Box>
           </Box>
-        </Box>
+        </div>
       </Box>
     </>
   );
